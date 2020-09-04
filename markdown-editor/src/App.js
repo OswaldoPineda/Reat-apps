84ef61b8ac0d72632ec196841e20077bc4739496
OswaldoPineda <carlos-1502010@hotmail.com>
2020-09-04 14:13:05 -0500

    Finish the markdown editor app

--- markdown-editor/src/App.js
+++ markdown-editor/src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import marked from 'marked';
 import './App.css';
 
 export default function App() {
+  const [markdown, setMarkdown] = useState('# ppppp');
+
+  const handleChange = (e) => {
+    setMarkdown(e.target.value);
+  };
+
   return (
     <div className="app">
-      <textarea />
+      <textarea value={markdown} onChange={handleChange}/>
 
-      <div className="preview" />
+      <div className="preview" dangerouslySetInnerHTML={{ __html: marked(markdown)}} />
     </div>
   );
 }
